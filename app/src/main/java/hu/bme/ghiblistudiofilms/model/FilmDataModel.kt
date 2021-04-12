03642e2099a5f85db74200f604d9ea09ee0e562e
Richárd Pasics <43377943+Wolvarr@users.noreply.github.com>
2021-04-12 14:07:18 +0200

    Added database annotations

--- app/src/main/java/hu/bme/ghiblistudiofilms/model/FilmDataModel.kt
+++ app/src/main/java/hu/bme/ghiblistudiofilms/model/FilmDataModel.kt
@@ -1,8 +1,12 @@
 package hu.bme.ghiblistudiofilms.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
+@Entity(tableName = "ModifiedFilms")
 data class FilmDataModel (
+        @PrimaryKey(autoGenerate = false) val id: UUID,
         val name: String,
         val originalName: String,
         val description: String,
--- app/src/main/java/hu/bme/ghiblistudiofilms/model/FilmListModel.kt
+++ app/src/main/java/hu/bme/ghiblistudiofilms/model/FilmListModel.kt
@@ -1,6 +1,10 @@
 package hu.bme.ghiblistudiofilms.model
 
+import java.util.*
+
+//Not saved to database
 data class FilmListModel(
+        val id: UUID,
         val name: String,
         val originalName: String
 )
